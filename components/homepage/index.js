160e098780571b31c670138f4ed023ef87abbb90
kidabu9x <duongnk.techkids@gmail.com>
2020-01-30 22:51:57 +0700

    update design homepage

--- components/homepage/index.js
+++ components/homepage/index.js
@@ -1,19 +1,86 @@
+/* eslint-disable react/prop-types */
 import React, { Fragment } from "react";
+import { makeStyles } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 
-import Section1 from "./components/Section1";
-import Section2 from "./components/Section2";
-import Section3 from "./components/Section3";
-import Section4 from "./components/Section4";
-import Section5 from "./components/Section5";
+import Banner4 from "../../assets/images/banner-4.jpg";
+import Banner5 from "../../assets/images/banner-5.jpg";
+
+const styles = makeStyles(theme => ({
+  root: {
+    backgroundImage: "url(" + Banner4 + ")",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    display: "flex",
+    flexWrap: "wrap",
+    height: "100vh"
+  },
+  pannelLeft: {
+    flex: "0 0 60%"
+  },
+  pannelRight: {
+    flex: "0 0 40%",
+    backgroundImage: "url(" + Banner5 + ")",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    color: "#ffffff",
+    padding: theme.spacing(8, 4),
+    boxSizing: "border-box"
+  },
+  btnLink: {
+    fontSize: "2.4rem",
+    color: "#fff",
+    textTransform: "unset",
+    marginTop: theme.spacing(2)
+  }
+}));
+
+const BtnLink = props => {
+  const { text, link, classes } = props;
+  return (
+    <div>
+      <Button
+        className={classes.btnLink}
+        variant="link"
+        color="primary"
+        href={link}
+      >
+        {text}
+      </Button>
+    </div>
+  );
+};
 
 const Home = () => {
+  const classes = styles();
   return (
     <Fragment>
-      <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section4 />
-      <Section5 />
+      <div className={classes.root}>
+        <div className={classes.pannelLeft}></div>
+        <div className={classes.pannelRight}>
+          <BtnLink
+            classes={classes}
+            link="http://study-hub.nimbus.edu.vn/"
+            text="Không gian học tập"
+          />
+          <BtnLink
+            classes={classes}
+            link="http://study-hub.nimbus.edu.vn/gioi-thieu"
+            text="Giới thiệu"
+          />
+          <BtnLink
+            classes={classes}
+            link="http://study-hub.nimbus.edu.vn/gioi-thieu"
+            text="IC3 - ICDL - MOS"
+          />
+          <BtnLink classes={classes} link="#" text="Sự kiện" />
+          <BtnLink
+            classes={classes}
+            link="https://blog.nimbus.edu.vn"
+            text="Blog"
+          />
+        </div>
+      </div>
     </Fragment>
   );
 };
--- layouts/default/index.js
+++ layouts/default/index.js
@@ -1,40 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core/styles";
 
 import Base from "../_base";
 
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    minHeight: "100vh"
-  },
-  main: {},
-  footer: {
-    padding: theme.spacing(3, 2),
-    marginTop: "auto",
-    backgroundColor:
-      theme.palette.type === "dark"
-        ? theme.palette.grey[800]
-        : theme.palette.grey[200]
-  }
-}));
-
 const Layout = props => {
-  const classes = useStyles();
-  return (
-    <Base>
-      <div className={classes.root}>
-        <NavBar />
-        <div className={classes.main}>{props.children}</div>
-        <Footer className={classes.footer} />
-      </div>
-    </Base>
-  );
+  // const classes = useStyles();
+  return <Base>{props.children}</Base>;
 };
 
 Layout.propTypes = {
